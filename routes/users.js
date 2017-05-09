f1f149e1e48ea890eb464a6d4f66bed82b852eed
Nikhil <nikhil.ithape@wwindia.com>
2017-05-09 12:00:03 +0530

    "Model Changes for Database Queries"

--- routes/users.js
+++ routes/users.js
@@ -4,9 +4,16 @@ var passport = require("passport");
 var LocalStrategy = require("passport-local").Strategy;
 var User = require("../Models/Users");
 
+var Users = {
+    username: string,
+    email: string,
+    password: string,
+    Id: number
+}
 router.get('/register', (req, res) => {
     res.render('Register');
 });
+
 
 router.post('/register', (req, res) => {
     var name = req.body.name;
@@ -26,8 +33,7 @@ router.post('/register', (req, res) => {
         res.render('register', {
             errors: errors
         })
-    }
-    else {
+    } else {
         var newUser = new Users({
             username: name,
             email: email,
@@ -50,8 +56,8 @@ router.get('/Login', (req, res) => {
 });
 
 passport.use(new LocalStrategy(
-    function (username, password, done) {
-        User.getUserByUsername(username, function (err, user) {
+    function(username, password, done) {
+        User.getUserByUsername(username, function(err, user) {
             if (err)
                 throw err;
             if (!user)
@@ -60,25 +66,25 @@ passport.use(new LocalStrategy(
     }
 ));
 
-passport.serializeUser(function (user, done) {
+passport.serializeUser(function(user, done) {
     done(null, user.id);
 });
 
-passport.deserializeUser(function (id, done) {
-    User.getUserById(id, function (err, user) {
+passport.deserializeUser(function(id, done) {
+    User.getUserById(id, function(err, user) {
         done(err, user);
     });
 });
 
 router.post('/login',
     passport.authenticate('local', { successRedirect: '/', failureRedirect: '/users/login', failureFlash: true }),
-    function (req, res) {
+    function(req, res) {
         res.redirect('/');
     });
 
 router.get('/Logout', (req, res) => {
     req.logOut();
-    req.flash("success_msg","Logged out");
+    req.flash("success_msg", "Logged out");
     res.redirect('/users/login');
 });
 
